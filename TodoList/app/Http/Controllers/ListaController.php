2b208cdc3a3a8f6a2de36deaadd39bff6cdc0b58
Evandro Ignacio <reignomo27@hotmail.com>
2019-04-23 12:16:37 -0300

    as actions index e store foram concluidas/Lista

--- TodoList/app/Http/Controllers/ListaController.php
+++ TodoList/app/Http/Controllers/ListaController.php
@@ -5,30 +5,37 @@ namespace App\Http\Controllers;
 use App\Lista;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
 
  
 class ListaController extends Controller{
 
     public function index(){
 
-        return response(Lista::all());
+        try{
+            $data =  Lista::all();
+            return response(array(['status' => 'sucess', 'code' => 200, 'data' => $data]));
+        } catch (QueryException $e) {
+            return response(array(['status' => 'error', 'code' => 503]));
+        }
     }
 
     public function store(Request $request){
 
-        $this->validate($request, [
-            'nome' => 'required|max:20'
-        ]);
-
+        $result = $this->validate($request, 
+            ['nome' => 'required|max:20'],
+            ['nome.required' => ['status' => 'error', 'code' => 400, 'message' => 'The nome field is required.', 'data' => null ]]
+        );
         $request = $request->json()->all();
-        $lista = new Lista();
-        $lista->nome = $request['nome'];
         try{
+            $lista = new Lista();
+            $lista->nome = $request['nome'];
             $lista->save();
-            return response(['201']);
+            $newList = Lista::all()->sortByDesc('created_at')->first();
+            return response(array(['status' => 'sucess', 'code' => 201, 'data' => $newList]));
         }
-        catch(Exception $e) {
-            return response(['400']);
+        catch(QueryException $e) {
+            return response(array(['status' => 'error', 'code' => 503, 'data' => null]));
         }
     }
 
