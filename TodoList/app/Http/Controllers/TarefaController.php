873aefb6d58d89bb787bf0f55f92eab851a72b10
Evandro Ignacio <reignomo27@hotmail.com>
2019-04-23 17:09:51 -0300

    ..

--- TodoList/app/Http/Controllers/TarefaController.php
+++ TodoList/app/Http/Controllers/TarefaController.php
@@ -6,21 +6,26 @@ use App\Tarefa;
 use App\Lista;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
  
 class TarefaController extends Controller{
 
     public function getTarefaById($id) {
        
-        $lista = Lista::find($id);
-        if(isset($lista['nome'])) {
-            $tarefas = Tarefa::all()->where('t_lista_id', $id);
-            if(isset($tarefas[0]['nome'])) {
-                return response(array(['status' => 'sucess', 'code' => 200, 'data' => $tarefas]));
+        try{
+            $lista = Lista::find($id);
+            if(isset($lista['nome'])) {
+                $tarefas = Tarefa::all()->where('t_lista_id', $id);
+                if(isset($tarefas[0]['nome'])) {
+                    return response(array(['status' => 'sucess', 'code' => 200, 'data' => $tarefas]));
+                }
+                return response(array(['status' => 'sucess', 'code' => 200, 'data' => 'dont have tarefas vinculas a esta lista']));
             }
-            return response(array(['status' => 'sucess', 'code' => 200, 'data' => 'dont have tarefas vinculas a esta lista']));
+            return response(array(['status' => 'error', 'message' => 'lista not found', 'code' =>  404]));
+        } catch(QueryException $e) {
+
+            return response(array(['status' => 'error', 'code' => 503]));
         }
-        return response(array(['status' => 'error', 'message' => 'lista not found', 'code' =>  '404']));
-        
     }
     
     public function getTarefaByNomeLista() {
